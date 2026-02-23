79bc6691966afcf4a288ad432ee982c9a4f5c94b
Taha <mohammedtahahalim@proton.me>
2026-02-23 09:57:45 +0000

    added support for multilang notifications

--- api/notifications.js
+++ api/notifications.js
@@ -1,5 +1,10 @@
 import auth from "../helpers/auth.js";
-import { notifications } from "../helpers/sample.js";
+import {
+  arNotifications,
+  enNotifications,
+  frNotifications,
+  jaNotifications,
+} from "../helpers/sample.js";
 
 export default async function handler(req, res) {
   if (req.method !== "GET")
@@ -7,10 +12,19 @@ export default async function handler(req, res) {
   try {
     const { unread, ...rest } = req.query;
     if (unread === "true") {
-      return res.status(200).json({ unreadCount: 0 });
+      return res
+        .status(200)
+        .json({ unreadCount: enNotifications.filter((n) => n.read).length });
     }
     auth(req, res);
-    return res.status(200).json({ notifications });
+    return res.status(200).json({
+      notifications: {
+        en: enNotifications,
+        ja: jaNotifications,
+        ar: arNotifications,
+        fr: frNotifications,
+      },
+    });
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal server error ..." });
