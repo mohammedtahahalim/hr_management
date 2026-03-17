d79fa331e8fdeebaf2f7b3b1a834d2d32c0b0e66
Taha <mohammedtahahalim@proton.me>
2026-03-17 12:35:53 +0000

    finished the main implementation of add-vacancy form

--- src/pages/vacancies/addVacancy/Contact.tsx
+++ src/pages/vacancies/addVacancy/Contact.tsx
@@ -1,6 +1,15 @@
 import type { UseFormRegister } from "react-hook-form";
-import { Box, styled } from "@mui/material";
+import {
+  Box,
+  styled,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  FormControlLabel,
+  Checkbox,
+} from "@mui/material";
 import type { NewVacancyData } from "./addVacancySlice";
+import { useTranslation } from "react-i18next";
 
 interface ContactProps {
   register: UseFormRegister<NewVacancyData>;
@@ -8,13 +17,83 @@ interface ContactProps {
 
 const ContactWrapper = styled(Box)({
   flex: 1,
-  border: "1px solid black",
+  padding: "15px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "15px",
+});
+
+const BasicFormControl = styled(FormControl)(({ theme }) => ({
+  width: "100%",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  gap: "25px",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    gap: "5px",
+  },
+}));
+
+const BasicFormLabel = styled(InputLabel)({
+  position: "static",
+  transform: "none",
+  fontFamily: "system-ui",
+  fontWeight: "bold",
+});
+
+const BasicFormInput = styled(OutlinedInput)({
+  flex: 1,
+  maxWidth: "400px",
+  borderRadius: "8px",
+  fontFamily: "system-ui",
 });
 
 export default function Contact({ register }: ContactProps) {
+  const { t } = useTranslation("addVacancy");
   return (
     <ContactWrapper>
-      <input type="text" {...register("hirer.name")} />
+      <BasicFormControl size="small">
+        <BasicFormLabel htmlFor="hirer-name">
+          {t("form.contact.person")}
+        </BasicFormLabel>
+        <BasicFormInput {...register("hirer.name")} id="hirer-name" />
+      </BasicFormControl>
+      <BasicFormControl size="small">
+        <BasicFormLabel htmlFor="hirer-phone">
+          {t("form.contact.phone")}
+        </BasicFormLabel>
+        <BasicFormInput
+          type="phone"
+          {...register("hirer.phone")}
+          id="hirer-phone"
+        />
+      </BasicFormControl>
+      <BasicFormControl size="small">
+        <BasicFormLabel htmlFor="additional-contact">
+          {t("form.contact.additional")}
+        </BasicFormLabel>
+        <BasicFormInput
+          {...register("hirer.additional")}
+          id="additional-contact"
+        />
+      </BasicFormControl>
+      <BasicFormControl size="small">
+        <BasicFormLabel htmlFor="show-info">
+          {t("form.contact.show")}
+        </BasicFormLabel>
+        <FormControlLabel
+          control={<Checkbox />}
+          label={t("form.contact.showLarge")}
+          sx={{
+            flex: 1,
+            maxWidth: "400px",
+            marginRight: "12px",
+          }}
+          id="show-info"
+        />
+      </BasicFormControl>
     </ContactWrapper>
   );
 }
--- src/pages/vacancies/addVacancy/Status.tsx
+++ src/pages/vacancies/addVacancy/Status.tsx
@@ -1,6 +1,15 @@
 import type { UseFormRegister } from "react-hook-form";
-import { Box, styled } from "@mui/material";
+import {
+  Box,
+  styled,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  TextField,
+} from "@mui/material";
 import type { NewVacancyData } from "./addVacancySlice";
+import { useTranslation } from "react-i18next";
 
 interface StatusProps {
   register: UseFormRegister<NewVacancyData>;
@@ -8,13 +17,89 @@ interface StatusProps {
 
 const StatusWrapper = styled(Box)({
   flex: 1,
-  border: "1px solid black",
+  padding: "15px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "15px",
+});
+
+const BasicFormControl = styled(FormControl)(({ theme }) => ({
+  width: "100%",
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-between",
+  alignItems: "flex-start",
+  gap: "25px",
+  [theme.breakpoints.down("sm")]: {
+    flexDirection: "column",
+    gap: "5px",
+  },
+}));
+
+const BasicFormLabel = styled(InputLabel)({
+  position: "static",
+  transform: "none",
+  fontFamily: "system-ui",
+  fontWeight: "bold",
+});
+
+const BasicFormSelect = styled(Select)({
+  flex: 1,
+  maxWidth: "400px",
+  borderRadius: "8px",
+  fontFamily: "system-ui",
+});
+
+const DatesControl = styled(Box)({
+  flex: 1,
+  maxWidth: "400px",
+  display: "flex",
+  justifyContent: "space-between",
+  flexWrap: "wrap",
+  gap: "10px",
 });
 
 export default function Status({ register }: StatusProps) {
+  const { t } = useTranslation("addVacancy");
   return (
     <StatusWrapper>
-      <input type="text" {...register("hirer.name")} />
+      <BasicFormControl size="small">
+        <BasicFormLabel htmlFor="vacancy-status">
+          {t("form.status.vacancy")}
+        </BasicFormLabel>
+        <BasicFormSelect
+          defaultValue={"open"}
+          {...register("job.status")}
+          id="vacancy-status"
+        >
+          <MenuItem value="open">{t("form.status.values.open")}</MenuItem>
+          <MenuItem value="completed">
+            {t("form.status.values.completed")}
+          </MenuItem>
+          <MenuItem value="inprogress">
+            {t("form.status.values.inprogress")}
+          </MenuItem>
+        </BasicFormSelect>
+      </BasicFormControl>
+      <BasicFormControl>
+        <BasicFormLabel htmlFor="openClose">
+          {t("form.status.openClose")}
+        </BasicFormLabel>
+        <DatesControl id="openClose">
+          <TextField
+            type="date"
+            {...register("job.openDate")}
+            size="small"
+            sx={{ maxWidth: "175px" }}
+          />
+          <TextField
+            type="date"
+            {...register("job.closeDate")}
+            size="small"
+            sx={{ maxWidth: "175px" }}
+          />
+        </DatesControl>
+      </BasicFormControl>
     </StatusWrapper>
   );
 }
