7fd8ca6d9f67688158b8da1bfd497487eba58cea
Taha <mohammedtahahalim@proton.me>
2026-03-23 14:20:57 +0100

    finised the layout and initial design of applicants page

--- src/pages/applicants/Applicants.tsx
+++ src/pages/applicants/Applicants.tsx
@@ -1,3 +1,11 @@
+import { Box, styled } from "@mui/material";
+
+const ApplicantsWrapper = styled(Box)({
+  width: "100%",
+  height: "100%",
+  border: "1px solid black",
+});
+
 export default function Applicants() {
-  return <div>Applicants</div>;
+  return <ApplicantsWrapper>Applicants</ApplicantsWrapper>;
 }
