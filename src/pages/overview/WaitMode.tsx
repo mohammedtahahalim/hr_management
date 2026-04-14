49cc150dbe24693b0bb8eee70f070258c4385952
Mohammed Taha HALIM <mohammedtahahalim@proton.me>
2026-04-14 20:55:06 +0100

    finished the stats section of overview

--- src/pages/overview/WaitMode.tsx
+++ src/pages/overview/WaitMode.tsx
@@ -1,10 +1,21 @@
 import type React from "react";
 import WithSkeleton from "../../shared/ui/WithSkeleton";
+import { useSelector } from "react-redux";
+import { selectOverviewStatus } from "./overviewSlice";
+import type { SxProps } from "@mui/material";
 
 interface WaitModeProps {
   children: React.ReactNode | React.ReactNode[];
+  sx?: SxProps;
 }
 
-export default function WaitMode({ children }: WaitModeProps) {
-  return <WithSkeleton loading={true}>{children}</WithSkeleton>;
+export default function WaitMode({ children, sx }: WaitModeProps) {
+  const status = useSelector(selectOverviewStatus);
+  const isLoading = status === "loading";
+
+  return (
+    <WithSkeleton loading={isLoading} sx={sx}>
+      {children}
+    </WithSkeleton>
+  );
 }
