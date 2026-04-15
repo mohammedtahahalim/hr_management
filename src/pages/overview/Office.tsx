afe4fe272d0a8a7c4fd1a01160a1fc34ed89d027
Mohammed Taha HALIM <mohammedtahahalim@proton.me>
2026-04-15 15:57:40 +0100

    finished the company offices segment

--- src/pages/overview/Office.tsx
+++ src/pages/overview/Office.tsx
@@ -1,12 +1,35 @@
 import { Box, styled } from "@mui/material";
+import Title from "../../shared/ui/Title";
+import { useTranslation } from "react-i18next";
+import type { TLanguage } from "../../config/i18n";
+import { MAP_LOCATION } from "../../shared/lib/constants";
 
 const OfficeWrapper = styled(Box)({
   flex: 1,
   minWidth: "350px",
-  border: "1px solid white",
   minHeight: "250px",
+  overflow: "hidden",
+  borderRadius: "12px",
+  padding: "10px",
+  display: "flex",
+  flexDirection: "column",
+  gap: "10px",
+});
+
+const Map = styled("iframe")({
+  width: "100%",
+  height: "100%",
+  borderRadius: "12px",
 });
 
 export default function Office() {
-  return <OfficeWrapper>Office</OfficeWrapper>;
+  const { t, i18n } = useTranslation("overview");
+  const lang = i18n.language as TLanguage;
+
+  return (
+    <OfficeWrapper>
+      <Title ender={false}>{t("map.title")}</Title>
+      <Map src={MAP_LOCATION[lang]} allowFullScreen={true} loading="lazy" />
+    </OfficeWrapper>
+  );
 }
